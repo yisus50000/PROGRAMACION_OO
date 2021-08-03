deaa5bf3a9b54c298d6d902fe92495b407c26e84
DELL <DELL@169.254.236.187>
2021-08-02 20:03:31 -0500

    taller

--- PROGRAMACION_OO/src/ejercicios/matrices/MainMatrices.java
+++ PROGRAMACION_OO/src/ejercicios/matrices/MainMatrices.java
@@ -4,7 +4,22 @@ public class MainMatrices {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    //1. Declarar una matriz A[5][4] de tipo char
+	char A[][] = new char[5][4];
+	
+	//2. Declarar una matriz B de tipo char con 5 filas y 6 columnas 
+	char B[][] = new char[5][6];
+	
+	//3. Declarar una matriz mimatriz de tipo long  con 2 filas y 4 columnas 
+	long mimatriz[][] = new long[2][4];
+	
+	//3. Declarar una matriz1 mimatriz de tipo long con 1 filas y 2 columnas 
+	// y asignar a la posicion [0][0] el valor de 2,la posicion [1][1] el valor de 8
+	//la posicion [2][2] el valor de 2
+	long mimatriz1[][] = new long[1][2];
+	mimatriz1[0][0] = 2;
+	mimatriz1[1][1] = 8;
+	mimatriz1[2][2] = 2;
 	}
 
 }
